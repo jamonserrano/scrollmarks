0052c3159094d38c9385f40c458b91c1082fa529
Viktor Honti <honti.viktor@gmail.com>
2017-12-02 16:17:59 +0100

    Add polyfill for CustomEvent

--- test/helpers/scroll.js
+++ test/helpers/scroll.js
@@ -1,4 +1,23 @@
-window.scrollWithEvent = function (y) {
-	window.scrollTo(0, y);
-	window.dispatchEvent(new Event('scroll'));
-}
+(function () {
+	// Trigger scroll event after scrolling
+	window.scrollWithEvent = function (y) {
+		window.scrollTo(0, y);
+		window.dispatchEvent(new CustomEvent('scroll'));
+	}
+	
+	// IE Polyfill for CustomEvent
+	// https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill	
+	if ( typeof window.CustomEvent === "function" ) return false;
+
+	function CustomEvent ( event, params ) {
+		params = params || { bubbles: false, cancelable: false, detail: undefined };
+		var evt = document.createEvent( 'CustomEvent' );
+		evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+		return evt;
+	}
+
+	CustomEvent.prototype = window.Event.prototype;
+
+	window.CustomEvent = CustomEvent;
+
+})();
