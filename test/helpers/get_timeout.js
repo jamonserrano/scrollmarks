afc25cfa3c67516a766fe9d4605a4c9ead8b4c20
Viktor Honti <honti.viktor@gmail.com>
2017-12-09 16:24:15 +0100

    Timeout tweak

--- test/helpers/get_timeout.js
+++ test/helpers/get_timeout.js
@@ -1,6 +1,6 @@
 (function () {
 	var isIosSafari = /iP(ad|hone|od).+Version\/[\d\.]+.*Safari/i.test(navigator.userAgent);
 	window.getTimeout = function (delay) {
-		return (Scrollmarks.config().scrollThrottle + (delay || 1)) / (isIosSafari ? 33 : 60) * 1000;
+		return Math.ceil((Scrollmarks.config().scrollThrottle + (delay || 1)) / (isIosSafari ? 33 : 60) * 1000);
 	}
 })();
