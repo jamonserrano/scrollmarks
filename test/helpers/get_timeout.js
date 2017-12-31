6f213b172fb9a084c3486bc421ff02f2fb83044c
jamonserrano <honti.viktor@gmail.com>
2017-12-31 12:46:07 +0100

    Adjust test timeouts

--- test/helpers/get_timeout.js
+++ test/helpers/get_timeout.js
@@ -1,6 +1,8 @@
 (function () {
 	// var isIosSafari = /iP(ad|hone|od).+Version\/[\d\.]+.*Safari/i.test(navigator.userAgent);
 	window.getTimeout = function (type) {
-		return Math.ceil((Scrollmarks.config()[(type || 'scroll') + 'Throttle'] + 10) / 60 * 1000);
+		// Theoretically delaying with 1 frame should be enough (and it is in Chrome Headless)
+		// but there are sporadic failures in Edge and FF so we add some leeway
+		return Math.ceil((Scrollmarks.config()[(type || 'scroll') + 'Throttle'] + 5) / 60 * 1000);
 	}
 })();
